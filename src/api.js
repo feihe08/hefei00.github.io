20514d67573c389320b1cd90a063e5feb3f4152d
hefei <hefei@zhugeio.com>
2017-03-07 00:07:08 +0800

    api

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import base64 from 'base64-url'
 const TOKEN = 'Nzg0ZmE0ZmU4ZDY2ZjU4ZDJjNzgyMGZhMGNiNDI5NGJkZTc0NTk2ZQ'
 
 export const github = axios.create({
-  baseURL: 'https://api.github.com/repos/xufei/blog/',
+  baseURL: 'https://api.github.com/repos/hefei00/blog/',
   timeout: 10000,
   headers: {
     'Authorization': `token ${base64.decode(TOKEN)}`
