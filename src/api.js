8b1b42fb1e748818be44622735f77e7fe05d7d84
hefei <hefei@zhugeio.com>
2017-04-05 20:22:25 +0800

    api请求加大限时为10s

--- src/api.js
+++ src/api.js
@@ -4,7 +4,7 @@ const TOKEN = 'MWZiZGIwM2I5ZTBmNmM5NDQxNWYwYWZmNWQ4YmJmYzFhMTVlN2YyYw'
 
 export const github = axios.create({
   baseURL: 'https://api.github.com/repos/hefei00/blog/',
-  timeout: 3000,
+  timeout: 10000,
   headers: {
     Authorization: `token ${base64.decode(TOKEN)}`
   }
