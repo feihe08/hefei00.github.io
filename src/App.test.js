236a3fd51869f12d1f177ba2bc767ad17b47ae6d
hefei <hefei@zhugeio.com>
2017-05-02 21:26:49 +0800

    简单的测试

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import blogApp from './reducers/index'
+
+let store = createStore(
+  blogApp,
+  applyMiddleware(thunk)
+)
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+  ReactDOM.render(<App store={store} />, div)
 })
