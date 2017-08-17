99f8ead385d29f48332b63f00d7748332061e0f1
hefei <hefei@zhugeio.com>
2017-08-18 07:51:06 +0800

    updrade to react@16

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import ReactLoading from 'react-loading'
 import './Loading.css'
 
 export default function Loading() {
   return (
     <div className="overlay">
-      <ReactLoading type='bubbles' color='#e3e3e3' />
+      <div className="loading">
+        Loading...
+      </div>
     </div>
   )
 }
