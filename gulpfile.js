aab41e7c01f19c20443d10744229b58f674b9989
hefei <hefei@zhugeio.com>
2017-09-11 22:02:24 +0800

    gh-pages

--- gulpfile.js
+++
@@ -1,7 +0,0 @@
-var gulp = require('gulp')
-var replace = require('gulp-replace');
-gulp.task('move', function () {
-  return gulp.src('./build/index.html')
-    .pipe(replace(/\/blog\//g, '\blog/build/'))
-    .pipe(gulp.dest(''))
-})
