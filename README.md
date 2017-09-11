765119a238e298e6b5c05038378cf0efc7c788da
hefei <hefei@zhugeio.com>
2017-09-11 22:01:01 +0800

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# my blog, powered by react
-
-[blog](https://hefei00.github.io/blog/)
--- src/log.js
+++
@@ -1,4 +0,0 @@
-export function log(str) {
-  let date = new Date()
-  console.log(date.toGMTString() + str)
-}
